8e9cd067c9bfd1d4c1dd4cfdfc8234d91ac5e4d7
Rhiana Heath <rhiana@heath.cc>
2014-03-21 23:08:31 +1100

    added gravatar to profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(profile)
+    gravatar_id = Digest::MD5::hexdigest(profile.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: profile.name, class: "gravatar")
+  end
+
 end
