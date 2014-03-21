51cef84615b8dc8dfa38c1d6c5ef32b97d42ec79
Rhiana Heath <rhiana@heath.cc>
2014-03-21 21:44:22 +1100

    starting mentorm me project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mentorme::Application.config.secret_key_base = 'c0f6cd3a5bec418208d0092b80a0e2a0e35c2367effefdef4d8746e1d43dbb76831dfd2a485b87e6fcde384439becaf1b115ded5ffec50f9feda83c4de928569'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mentorme::Application.config.session_store :cookie_store, key: '_mentorme_session'
