33705c673276a5553eb744a49fb749d55cddd011
Rhiana Heath <rhiana@heath.cc>
2014-05-10 16:22:12 +1000

    addes search to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Mentorme::Application.routes.draw do
-  resources :profiles
+  resources :profiles do
+    collection do
+      get :search
+    end
+  end
 
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
